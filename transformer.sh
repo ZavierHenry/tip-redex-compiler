717b4c912658ffbf4901a99fe4cfe9628d0d6fc8
ZavierHenry <10481844+ZavierHenry@users.noreply.github.com>
2017-11-13 21:51:56 -0600

    Added new transformer shell file

--- transformer.sh
+++ transformer.sh
@@ -5,26 +5,31 @@ case "$1" in
     build)
         case "$2" in
         *.smt2)
+            tfile=$(mktemp "XXXXXXXXXXX.rkt")
+            sed '1s|^|#lang s-exp "tip-syntax.rkt"\n|' "$2" > "$tfile"
             case "$3" in
-            *.rkt)
-            cp "$2" "$3"
-            sed -i '1s/^/#lang s-exp "tip-syntax.rkt"\n/' "$3"
-            raco expand "$3" > "$3"
-            ;;
+            *.exe|*.app)
+                raco exe -o "$3" "$tfile"
+                ;;
+            *.*)
+                printf "\nUnsupported file: $3\n"
+                printf "See '$0 build --help' for more help on build usage.\n"
+                ;;
             *)
-            raco exe -o "$3" "$2"
-            raco distribute "src" "$2"
-            ;;
+                raco exe -o "$3" "$tfile"
+                ;;
             esac
+            rm "$tfile"
             ;;
         --help)
-            printf "\n\nUsage:\t$0 build [--help] [SOURCE FILE] [DESTINATION FILE]\n\n"
+            printf "\n\nUsage:\t$0 build [flags] [SOURCE] [DESTINATION]\n\n"
             printf "Builds the benchmark as a file type determinted by the destination filename format.\n\n"
             printf "Supported file types:\n\n"
             printf "\tNo file extension (Unix executable)\n"
-            printf "\t.rkt (Racket file)\n"
             printf "\t.exe (Windows executable)\n"
-            printf "\t.app (Apple application)\n"
+            printf "\t.app (Apple application)\n\n"
+            printf "List of flag options:\n\n"
+            printf "\t--help:\tShows this help page\n\n"
             ;;
         *)
             printf "\nUnsupported source file.\n\nSee '$0 build --help' for more help on build usage.\n"
@@ -32,9 +37,20 @@ case "$1" in
         esac
     ;;
     run)
-        output=&1
+        output=""
         verbose=false
         timeset=false
+        
+        case "$2" in
+        --help)
+            printf "\n\nUsage:\t$0 run [flags and options] ...\n\n"
+            printf "List of flags:\n\n"
+            printf "\t-t [--time]:\t times the execution of the benchmark.\n"
+            printf "\t-l [--log=FILEPATH]: Logs results to the given file.\n"
+            printf "\t-v [--verbose]:\tPrints all results (only failed results are shown by default)\n"
+            exit
+            ;;
+        esac
         
         while [[ $# -gt 2 ]]
         do
@@ -44,21 +60,16 @@ case "$1" in
                 shift
                 ;;
             -l)
+                output="$3"
                 shift 2
                 ;;
             --log=*)
+                output="${2##--log=}"
                 shift
                 ;;
             -t|--time)
                 timeset=true
                 shift
-            ;;
-            --help)
-                printf "\n\nUsage:\t$0 run [flags and options] ...\n\n"
-                printf "List of flags:\n\n"
-                printf "\t-t [--time]:\t times the execution of the benchmark.\n"
-                printf "\t-l [--log=FILEPATH]: Logs results to the given file.\n"
-                exit
                 ;;
             *)
                 printf "\nUnknown run flag.\n\nSee '$0 run --help' for more help on run usage.\n"
@@ -67,13 +78,26 @@ case "$1" in
             esac
         done
         
+        src=$(mktemp XXXXXXXXXXXX.rkt)
+        sed '1s|^|#lang s-exp "tip-syntax.rkt"|' "$2" > "$src"
+        
         if $timeset; then
-            time racket "$2" &1>"$output" &2>1
+            result=$( { time racket "$src" 2>&1 ; } 2>&1)
         else
-            racket "$2" 1>"$output" 2>&1
+            result=$(racket "$src" 2>&1)
         fi
         
-        # remove .dep, .bak, and .zo files
+        if $verbose && [[ -n "$output" ]]; then
+            echo "$result" >> "$output"
+        elif $verbose; then
+            echo "$result"
+        elif [[ ! "$result" =~ "no counterexamples" && output ]]; then
+            echo "$result" >> "$output"
+        elif [[ ! "$result" =~ "no counterexamples" ]]; then
+            echo "$result"
+        fi
+        
+        find . -type f -name "${src%%.rkt}\.*" -delete
         ;;
     help)
         printf "\n\nUsage:\t$0 command [arguments] FILEPATH\n\n"
